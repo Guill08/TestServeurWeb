19a916cc14758ef99e34b454d805b1a2cd20308f
rsi@BAZEILLES.local <guillaume.suppin@hellomoov.com>
2024-10-10 09:23:36 +0200

    init

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
             HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 8001), 0);
 
             server.createContext("/test", new  MyHttpHandler());
-            server.setExecutor(threadPoolExecutor);
+
             server.start();
 
 

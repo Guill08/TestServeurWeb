6db4c64b3e7e94156eb662374bfe12d973ad3c2d
rsi@BAZEILLES.local <guillaume.suppin@hellomoov.com>
2024-10-10 09:17:00 +0200

    init

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 8001), 0);
+
+            server.createContext("/test", new  MyHttpHandler());
+            server.setExecutor(threadPoolExecutor);
+            server.start();
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
